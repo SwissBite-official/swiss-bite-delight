5c9b3f97f26a801ebbe87e23e499dfadf2f88e1e
Daniel Košč <daniel.kosc@gmail.com>
2025-02-03 17:24:01 +0100

    Adding ingredients table

--- src/pages/Branches.tsx
+++ src/pages/Branches.tsx
@@ -36,7 +36,6 @@ const Branches = () => {
                         <section>
                             <h2 className="text-2xl font-semibold text-swiss-red mb-4">Production Process</h2>
                             <p className="text-gray-700">{t('products.branches.production')}</p>
-                            <p className="text-gray-700 mt-2">{t('products.branches.ingredients')}</p>
                         </section>
 
                         <section>
@@ -57,6 +56,55 @@ const Branches = () => {
                         <section>
                             <h2 className="text-2xl font-semibold text-swiss-red mb-4">Ingredients</h2>
                             <p className="text-gray-700">{t('products.branches.ingredients')}</p>
+                        </section>
+
+                        <section className="flex justify-center">
+                            <table className="w-full max-w-4xl border-collapse border border-gray-300">
+                                <thead>
+                                <tr className="bg-gray-200">
+                                    <th className="border border-gray-300 p-2">{t('products.branches.nutrition_values')}</th>
+                                    <th className="border border-gray-300 p-2">100 g</th>
+                                    <th className="border border-gray-300 p-2">{t('products.branches.portion')} (17 g)</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td className="border border-gray-300 p-2">{t('products.branches.energy')}</td>
+                                    <td className="border border-gray-300 p-2">2242 kJ / 537 kcal</td>
+                                    <td className="border border-gray-300 p-2">381 kJ / 91 kcal (4,5%*)</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-gray-300 p-2">{t('products.branches.fat')}</td>
+                                    <td className="border border-gray-300 p-2">30 g</td>
+                                    <td className="border border-gray-300 p-2">5,1 g (7%*)</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-gray-300 p-2 pl-4">{t('products.branches.fat_acid')}</td>
+                                    <td className="border border-gray-300 p-2">18 g</td>
+                                    <td className="border border-gray-300 p-2">3,1 g (15%*)</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-gray-300 p-2">{t('products.branches.Carbohydrates')}</td>
+                                    <td className="border border-gray-300 p-2">61 g</td>
+                                    <td className="border border-gray-300 p-2">10 g (4%*)</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-gray-300 p-2 pl-4">{t('products.branches.sugar')}</td>
+                                    <td className="border border-gray-300 p-2">56 g</td>
+                                    <td className="border border-gray-300 p-2">9,5 g (11%*)</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-gray-300 p-2">Protein</td>
+                                    <td className="border border-gray-300 p-2">4,8 g</td>
+                                    <td className="border border-gray-300 p-2">0,8 g (1,5%*)</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-gray-300 p-2">{t('products.branches.salt')}</td>
+                                    <td className="border border-gray-300 p-2">0,16 g</td>
+                                    <td className="border border-gray-300 p-2">0,03 g (&lt;1%*)</td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </section>
                     </div>
                 </motion.div>
--- src/pages/DarkChocolate.tsx
+++ src/pages/DarkChocolate.tsx
@@ -36,7 +36,6 @@ const DarkChocolate = () => {
             <section>
               <h2 className="text-2xl font-semibold text-swiss-red mb-4">Production Process</h2>
               <p className="text-gray-700">{t('products.darkChocolate.production')}</p>
-              <p className="text-gray-700 mt-2">{t('products.darkChocolate.ingredients')}</p>
             </section>
 
             <section>
@@ -57,6 +56,54 @@ const DarkChocolate = () => {
             <section>
               <h2 className="text-2xl font-semibold text-swiss-red mb-4">Ingredients</h2>
               <p className="text-gray-700">{t('products.darkChocolate.ingredients')}</p>
+            </section>
+            <section className="flex justify-center">
+              <table className="w-full max-w-4xl border-collapse border border-gray-300">
+                <thead>
+                <tr className="bg-gray-200">
+                  <th className="border border-gray-300 p-2">{t('products.darkChocolate.nutrition_values')}</th>
+                  <th className="border border-gray-300 p-2">100 g</th>
+                  <th className="border border-gray-300 p-2">{t('products.darkChocolate.portion')} (17 g)</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td className="border border-gray-300 p-2">{t('products.darkChocolate.energy')}</td>
+                  <td className="border border-gray-300 p-2">2239 kJ / 537 kcal</td>
+                  <td className="border border-gray-300 p-2">381 kJ / 91 kcal (4,5%*)</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">{t('products.darkChocolate.fat')}</td>
+                  <td className="border border-gray-300 p-2">32 g</td>
+                  <td className="border border-gray-300 p-2">5,4 g (8%*)</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2 pl-4">{t('products.darkChocolate.fat_acid')}</td>
+                  <td className="border border-gray-300 p-2">19 g</td>
+                  <td className="border border-gray-300 p-2">3,2 g (16%*)</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">{t('products.darkChocolate.Carbohydrates')}</td>
+                  <td className="border border-gray-300 p-2">54 g</td>
+                  <td className="border border-gray-300 p-2">9,2 g (3,5%*)</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2 pl-4">{t('products.darkChocolate.sugar')}</td>
+                  <td className="border border-gray-300 p-2">51 g</td>
+                  <td className="border border-gray-300 p-2">8,7 g (10%*)</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">Protein</td>
+                  <td className="border border-gray-300 p-2">4,8 g</td>
+                  <td className="border border-gray-300 p-2">0,8 g (1,5%*)</td>
+                </tr>
+                <tr>
+                  <td className="border border-gray-300 p-2">{t('products.darkChocolate.salt')}</td>
+                  <td className="border border-gray-300 p-2">0,01 g</td>
+                  <td className="border border-gray-300 p-2">&lt;0,01 g (&lt;1%*)</td>
+                </tr>
+                </tbody>
+              </table>
             </section>
           </div>
         </motion.div>
